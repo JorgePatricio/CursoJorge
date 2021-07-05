b667feda9ffa82eb9e81fa851b599106ece7626a
Jorge Patricio <jorgegspatricio@gmail.com>
2021-07-05 13:13:13 -0300

    Projeto iniciado dos exercicios

---
+++ ExercicioClasses/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace ExercicioClasses {
+    class ConversorDeMoeda {
+
+        public static double Cotacao, Dolar;
+
+        public static double CalculoDolarReal(double dolar) {
+            return ((dolar * 0.06)+dolar) * Cotacao;
+        }
+
+    }
+}
+
---
+++ ExercicioClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClasses {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual é a cotação do dolar? ");
+            ConversorDeMoeda.Cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Qual dolares você irá comprar? ");
+            ConversorDeMoeda.Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Valor a ser pago em reais: R$ " + ConversorDeMoeda.CalculoDolarReal(ConversorDeMoeda.Dolar).ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
